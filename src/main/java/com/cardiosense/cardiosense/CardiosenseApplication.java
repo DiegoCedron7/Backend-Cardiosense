51b222faf7a12ac79f467c0bb3d1fdc052bcb383
Your Name <Your Name>
2024-10-01 18:41:03 -0500

    primer commit

---
+++ src/main/java/com/cardiosense/cardiosense/CardiosenseApplication.java
@@ -0,0 +1,13 @@
+package com.cardiosense.cardiosense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardiosenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardiosenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardiosense/cardiosense/CardiosenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardiosense.cardiosense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardiosenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
