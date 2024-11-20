ff84681545fc685206d6a951016840625b0f27e8
Diego Cedron <70548640+DiegoCedron7@users.noreply.github.com>
2024-11-19 19:44:51 -0500

    feat: add keep alive service and schedule func

---
+++ src/main/java/com/cardiosense/cardiosense/service/KeepAliveService.java
@@ -0,0 +1,29 @@
+package com.cardiosense.cardiosense.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@AllArgsConstructor
+public class KeepAliveService {
+
+    private final RestTemplate restTemplate;
+
+    @Scheduled(fixedRate = 600000)
+    public void sendKeepAlive() {
+        String url = "";
+        try {
+            restTemplate.getForObject(url, String.class);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            System.out.println("KeepAlive ping at: " + dtf.format(now));
+        } catch (Exception e) {
+            System.err.println("Error during KeepAlive ping: " + e.getMessage());
+        }
+    }
+}
